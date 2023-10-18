ff914b8378401a9f66b553a7c5b19723148f691c
ctapiahijo <ctapiahijo@yahoo.es>
2023-10-17 21:43:12 -0400

    Move class out of main.

---
+++ DynamicDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleDatabaseViewer
+{
+    internal class DynamicDatabase : DbContext
+    {
+        public string? dbName;
+        public string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public string dbPath;
+
+        public DynamicDatabase() 
+        {
+
+            Console.Write("Enter the name for the new database: ");
+            dbName = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(dbName))
+            {
+                Console.WriteLine("Database name cannot be empty. Exiting.");
+                Environment.Exit(1);
+            }
+
+            dbPath = Path.Combine(desktopPath, $"{dbName}.db");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source = {dbPath}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,33 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 
 namespace ConsoleDatabaseViewer
 {
-
-    internal class DynamicDatabase : DbContext
-    {
-        public string? dbName;
-        public string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        public string dbPath;
-
-        public DynamicDatabase() 
-        {
-
-            Console.Write("Enter the name for the new database: ");
-            dbName = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(dbName))
-            {
-                Console.WriteLine("Database name cannot be empty. Exiting.");
-                Environment.Exit(1);
-            }
-
-            dbPath = Path.Combine(desktopPath, $"{dbName}.db");
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite($"Data Source = {dbPath}");
-        }
-    }
     internal class Program
     {
         static async Task Main(string[] args)
